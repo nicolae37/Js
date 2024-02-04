1cda322dc6542ab4abd87c84824a499796dc4a0f
nicolae37 <153417823+nicolae37@users.noreply.github.com>
2024-02-04 11:01:22 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,25 @@
+let a = document.getElementById("Input1").value;
+let b = document.getElementById("Input2").value;
+let c = document.getElementById("InputOp").value;
+
+function CalcPlus(CalcPLus){
+    let adunare; 
+    adunare = a + b
+}
+
+function CalcMinus(CalcMinus){
+    let scadere;
+    scadere = a - b
+}
+
+function CalcMulti(CalcMulti){
+    let inmultire;
+    inmultire = a * b
+}
+
+function CalcDiv(CalcDiv){
+    let impartire;
+    impartire = a / b
+}
+
+let result = null
